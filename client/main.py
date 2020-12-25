0733c6ab71a4aa5378fcfb77dd1f5c3637a76767
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-12-25 19:55:09 +0400

    added some simple server and client communication

--- client/main.py
+++ client/main.py
@@ -1,18 +1,52 @@
+from logging import fatal
+import socket
+import threading
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = 8000
+APPLICATION_STOPPED = False
 
 
-class MainGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MainGrid, self).__init__(**kwargs)
-        self.cols = 1
+def check_for_message():
+    print("Listening for messsages...")
 
-        self.add_widget(Label(
-            text="STEM App\nThis app will work with the other device to notify you if you left your child in your car."
-        ))
+    while True:
+        if APPLICATION_STOPPED:
+            return
+
+        msg = s.recv(2048).decode("utf-8")
+        print(msg)
+
+
+class MainGrid(Widget):
+    ip = ObjectProperty(None)
+
+    def btn(self):
+        print(f"Attempting to establish a connection with {self.ip.text}...")
+
+        try:
+            s.connect((self.ip.text, PORT))
+        except ConnectionRefusedError:
+            print("Server has not started!")
+            return
+        except socket.gaierror:
+            print("Invalid IP address!")
+            return
+        except OSError:
+            print("Invalid IP address!")
+            return
+
+        msg = s.recv(2048).decode("utf-8")
+
+        if msg == "CONNECTED":
+            self.ip.text = ""
+            print("Connection established!")
+            msg_thread = threading.Thread(target=check_for_message)
+            msg_thread.start()
 
 
 class STEMApp(App):
@@ -21,4 +55,7 @@ class STEMApp(App):
 
 
 if __name__ == "__main__":
+    # TODO: Program a way to notify server that client has stopped
     STEMApp().run()
+    APPLICATION_STOPPED = True
+    s.close()
--- server/main.py
+++ server/main.py
@@ -1,1 +1,25 @@
-print("Coming Soon...")
+import socket
+import urllib.request
+
+ADDR = '127.0.0.1'
+PORT = 8000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((ADDR, PORT))
+s.listen(2)
+print("Listening for connections at:")
+print("IP:", urllib.request.urlopen('https://ident.me').read().decode('utf8'), "Port:", PORT)
+
+
+def new_client():
+    while True:
+        clientsocket, clientaddr = s.accept()
+        clientsocket.send(b"CONNECTED")
+
+
+def main():
+    client = new_client()
+
+
+if __name__ == "__main__":
+    main()
