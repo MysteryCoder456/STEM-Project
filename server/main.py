627ce80b96ea0bcdfa2a5f70e284cfcc369e6dde
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-12-29 18:29:35 +0400

    stop looking for sign when client disconnects

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ import cv2
 
 ADDR = '127.0.0.1'
 PORT = 8000
-APPLICATION_STOPPED = False
+CONNECTED = False
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((ADDR, PORT))
@@ -16,21 +16,29 @@ CLIENT = None
 
 
 def new_client():
+    global CONNECTED
+
     print("Listening for connections at:")
     print("IP:", urllib.request.urlopen('https://ident.me').read().decode('utf8'), "Port:", PORT)
+
+    # Wait for a new connection
     clientsocket, clientaddr = s.accept()
+
     clientsocket.send(b"CONNECTED")
     print(f"Client from {clientaddr} has established a connection!")
+    CONNECTED = True
+
     return clientsocket
 
 
 def listen_for_messages():
-    global CLIENT
+    global CLIENT, CONNECTED
 
     while True:
         msg = CLIENT.recv(2048).decode("utf-8")
 
         if msg == "QUIT":
+            CONNECTED = False
             print("Connection closed by client!")
             CLIENT = new_client()
 
@@ -50,21 +58,22 @@ def main():
 
         # Broadcasting messages
         while True:
-            cam_available, img = cap.read()
+            if CONNECTED:
+                cam_available, img = cap.read()
 
-            # Camera operation
-            if cam_available:
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                faces = cascade_classifier.detectMultiScale(gray, minSize=(50, 50), minNeighbors=4)
+                # Camera operation
+                if cam_available:
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    faces = cascade_classifier.detectMultiScale(gray, minSize=(50, 50), minNeighbors=4)
 
-                for (pos_x, pos_y, width, height) in faces:
-                    cv2.rectangle(img, (pos_x, pos_y), (pos_x + width, pos_y + height), (255, 0, 0), 3)
+                    for (pos_x, pos_y, width, height) in faces:
+                        cv2.rectangle(img, (pos_x, pos_y), (pos_x + width, pos_y + height), (255, 0, 0), 3)
 
-                cv2.imshow("Camera Output", img)
+                    cv2.imshow("Camera Output", img)
 
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    cap.release()
-                    break
+                    if cv2.waitKey(1) & 0xFF == ord("q"):
+                        cap.release()
+                        break
 
     except KeyboardInterrupt:
         cap.release()
