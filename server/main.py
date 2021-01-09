9092863bbce04dd0c2c6afcf7ba8c30109b1f355
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-09 15:02:11 +0400

    client is not alerted anymore when a trusted guardian is in the vehicle

--- server/main.py
+++ server/main.py
@@ -3,6 +3,7 @@ Server script that runs on the helper device. Device must have a either a camera
 Run this script from inside the server folder.
 """
 
+import os
 import sys
 import time
 import socket
@@ -12,9 +13,11 @@ import urllib.request
 import cv2
 import pyaudio
 import numpy as np
+import face_recognition
 
 CAMERA_PREVIEW = ("--camera-preview" in sys.argv)
 SOUND_PREVIEW = ("--sound-preview" in sys.argv)
+DISABLE_MIC = ("--disable-mic" in sys.argv)
 
 SOUND_THRESHOLD = 160
 
@@ -35,6 +38,10 @@ s.listen(2)
 
 CLIENT = None
 sending_message = False
+
+# Facial Recognition stuff
+known_guard_faces = []
+known_guard_names = []
 
 
 def new_client():
@@ -88,18 +95,28 @@ def main():
 
     # Initialize OpenCV stuff
     cap = cv2.VideoCapture(0)
-    cascade_classifier = cv2.CascadeClassifier("haar_cascade_face.xml")
 
     # Initialize audio stuff
-    p = pyaudio.PyAudio()
-    stream = p.open(
-        rate=RATE,
-        channels=CHANNELS,
-        format=FORMAT,
-        input=True,
-        output=True,
-        frames_per_buffer=CHUNK
-    )
+    if not DISABLE_MIC:
+        p = pyaudio.PyAudio()
+        stream = p.open(
+            rate=RATE,
+            channels=CHANNELS,
+            format=FORMAT,
+            input=True,
+            output=True,
+            frames_per_buffer=CHUNK
+        )
+
+    # Initialize face recog stuff
+    for face_name in os.listdir("known_guardians"):
+        if face_name != ".DS_Store":
+            for filename in os.listdir(f"known_guardians/{face_name}"):
+                if filename != ".DS_Store":
+                    image = face_recognition.load_image_file(f"known_guardians/{face_name}/{filename}")
+                    encoding = face_recognition.face_encodings(image)[0]
+                    known_guard_faces.append(encoding)
+                    known_guard_names.append(face_name)
 
     CLIENT = new_client()
 
@@ -112,21 +129,28 @@ def main():
             if CONNECTED:
                 # Camera operation
                 people_detected = False
+                guardian_detected = False
 
                 cam_available, img = cap.read()
 
                 if cam_available:
-                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    faces = cascade_classifier.detectMultiScale(gray, scaleFactor=1.3, minSize=(100, 100), minNeighbors=4)
-
-                    if len(faces) > 0:
-                        print(f"{len(faces)} faces were detected!")
+                    face_locations = face_recognition.face_locations(img)
+                    face_encodings = face_recognition.face_encodings(img, face_locations)
+                    for f_enc, f_loc in zip(face_encodings, face_locations):
+                        results = face_recognition.compare_faces(known_guard_faces, f_enc, tolerance=0.7)
+                        guardian_detected = (True in results)
                         people_detected = True
 
-                    if CAMERA_PREVIEW:
-                        for (pos_x, pos_y, width, height) in faces:
-                            cv2.rectangle(img, (pos_x, pos_y), (pos_x + width, pos_y + height), (255, 0, 0), 3)
+                        if CAMERA_PREVIEW:
+                            top_left = (f_loc[3], f_loc[0])
+                            bottom_right = (f_loc[1], f_loc[2])
+                            if True in results:
+                                color = (0, 255, 0)
+                            else:
+                                color = (255, 0, 0)
+                            cv2.rectangle(img, top_left, bottom_right, color, 3)
 
+                    if CAMERA_PREVIEW:
                         cv2.imshow("Camera Output", img)
 
                     if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -134,14 +158,15 @@ def main():
                         return
 
                 # Sound operation
-                sound_data = struct.unpack(f"{2 * CHUNK}B", stream.read(CHUNK))
-                volume = np.linalg.norm(sound_data)
-                print(volume)
+                if not DISABLE_MIC:
+                    sound_data = struct.unpack(f"{2 * CHUNK}B", stream.read(CHUNK))
+                    volume = np.linalg.norm(sound_data)
+                    print(volume)
 
-                if volume > SOUND_THRESHOLD:
-                    people_detected = True
+                    if volume > SOUND_THRESHOLD:
+                        people_detected = True
 
-                if people_detected and not sending_message:
+                if people_detected and not guardian_detected and not sending_message:
                     pd_thread = threading.Thread(target=person_detected, daemon=True)
                     pd_thread.start()
 
